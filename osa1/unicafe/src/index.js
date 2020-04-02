e0f9476b79f662594cbbd8b5991d5fe648476c48
Timo Varila <timo.varila@siili.com>
2020-04-02 16:31:22 +0300

    step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -5,26 +5,36 @@ const Button = ({handleClick, text}) => <button onClick={handleClick}> {text} </
 
 const Display = ({value, text}) => <p>{text} {value}</p>
 
+const Statistics = (props) => {
+  const {good, neutral, bad} = props;
+  return (
+    <>
+      <h1>statistics</h1>
+      <Display value={good} text={'good'} />
+      <Display value={neutral} text={'neutral'} />
+      <Display value={bad} text={'bad'} />
+      <Display value={good+neutral+bad} text={'all'} />
+      <Display value={(good-bad)/(good+neutral+bad)} text={'average'} />
+      <Display value={`${100*good/(good+neutral+bad)}%`} text={'positive'} />
+    </>
+  );  
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] =useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
-  const [feedbackSum, setFeedbackSum] =useState(0);
   
   const addGood = () => {
     setGood(good + 1);
-    setAll(all + 1);
-    setFeedbackSum(feedbackSum + 1);
   }
+
   const addNeutral = () => {
     setNeutral(neutral + 1);
-    setAll(all + 1);
   }
+
   const addBad = () => {
     setBad(bad + 1);
-    setAll(all + 1);
-    setFeedbackSum(feedbackSum - 1);
   }
 
   return (
@@ -35,14 +45,7 @@ const App = () => {
       <Button handleClick={addNeutral} text={'neutral'} />
       <Button handleClick={addBad} text={'bad'} />
       <br />
-      <h1>statistics</h1>
-      <Display value={good} text={'good'} />
-      <Display value={neutral} text={'neutral'} />
-      <Display value={bad} text={'bad'} />
-      <Display value={all} text={'all'} />
-      <Display value={feedbackSum/all} text={'average'} />
-      <Display value={`${100*good/all}%`} text={'positive'} />
-
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
