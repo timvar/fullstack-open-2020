9eb88ce65a4f44123cc1129a548afe737da36884
Timo Varila <timo.varila@siili.com>
2020-04-02 15:39:07 +0300

    step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({handleClick, text}) => <button onClick={handleClick}> {text} </button>
+
+const Display = ({value, text}) => <p>{text} {value}</p>
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] =useState(0);
   const [bad, setBad] = useState(0);
-
+  
+  const addGood = () => {setGood(good + 1)}
+  const addNeutral = () => {setNeutral(neutral + 1)}
+  const addBad = () => {setBad(bad + 1)}
+  
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <br />
+      <Button handleClick={addGood} text={'good'} />
+      <Button handleClick={addNeutral} text={'neutral'} />
+      <Button handleClick={addBad} text={'bad'} />
+      <br />
+      <h1>statistics</h1>
+      <Display value={good} text={'good'} />
+      <Display value={neutral} text={'neutral'} />
+      <Display value={bad} text={'bad'} />
     </div>
   )
 }
