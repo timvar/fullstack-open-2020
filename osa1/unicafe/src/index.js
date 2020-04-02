8ce64d0eb08df06cfd2545abf4fbb1f55b8fb89b
Timo Varila <timo.varila@siili.com>
2020-04-02 15:19:52 +0300

    step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] =useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
