e17639c4a1db49c38a450fefa72b9db300f4e308
Timo Varila <timo.varila@siili.com>
2020-04-02 22:43:23 +0300

    step6

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {}
+const Header = ({course}) => <h1>{course.name}</h1>
+
+const Content = ({course}) => {
+  return (
+    <>
+      {course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+    </>
+  )
+}
+
+const Part = ({name, exercises}) => <p> {name} {exercises} </p>
+
+const Course = ({course}) => {
+ return (
+   <>
+    <Header course={course} />
+    <Content course={course} />
+   </>
+ )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
-
