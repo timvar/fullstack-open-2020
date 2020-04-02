becbe1c67704fd1f4e37af0e69704cfb7bde8f29
Timo Varila <timo.varila@siili.com>
2020-04-02 16:14:54 +0300

    step2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,11 +9,24 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] =useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [feedbackSum, setFeedbackSum] =useState(0);
   
-  const addGood = () => {setGood(good + 1)}
-  const addNeutral = () => {setNeutral(neutral + 1)}
-  const addBad = () => {setBad(bad + 1)}
-  
+  const addGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+    setFeedbackSum(feedbackSum + 1);
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  }
+  const addBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+    setFeedbackSum(feedbackSum - 1);
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,6 +39,10 @@ const App = () => {
       <Display value={good} text={'good'} />
       <Display value={neutral} text={'neutral'} />
       <Display value={bad} text={'bad'} />
+      <Display value={all} text={'all'} />
+      <Display value={feedbackSum/all} text={'average'} />
+      <Display value={`${100*good/all}%`} text={'positive'} />
+
     </div>
   )
 }
