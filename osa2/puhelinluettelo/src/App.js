c57ad512c7332cfd30cc1425314a2983de5f757b
Timo Varila <timo.varila@siili.com>
2020-04-06 10:26:23 +0300

    step8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Person = ({ person }) => <p> {person.name} {person.number} </p>
 
@@ -42,8 +42,8 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -68,9 +68,10 @@ const App = () => {
         name: newName,
         number: newNumber
       };
-      axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then(response => {
+
+      personService
+        .create(newPerson)
+        .then(response => {
           setPersons(persons.concat(response.data));
         })
         .catch(error => alert('Failed to add person to database.'));
