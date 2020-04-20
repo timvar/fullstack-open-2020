a89891baff75247c6681293cac58a0b24ed6610e
Timo Varila <timo.varila@siili.com>
2020-04-20 16:24:36 +0300

    6.5 step3

--- osa6/redux-anecdotes/src/components/Anecdotes.js
+++ osa6/redux-anecdotes/src/components/Anecdotes.js
@@ -7,7 +7,7 @@ const Anecdotes = ({vote}) => {
   return (
     <>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
