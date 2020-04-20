4ac20603f5a896a2b366dd0744c6fb6fc3309a3f
Timo Varila <timo.varila@siili.com>
2020-04-20 17:05:40 +0300

    6.9 step7

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -5,7 +5,7 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
