02f54a6482d97301fd2a525f591754cc7717b556
Timo Varila <timo.varila@siili.com>
2020-04-23 11:16:45 +0300

    8.8 and 8.9

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,52 @@
+  
+import React from 'react'
+import { gql, useQuery} from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = ({show}) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!show) {
+    return null
+  }
+
+  const authors = result.data ? result.data.allAuthors : []
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
---
+++ osa8/library-frontend/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+const query = gql`
+  query {
+    allBooks {
+      title
+      author
+    }
+  }
+`
+
+client.query({ query })
+  .then(response => {
+    console.log(response.data)
+  })
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+)
