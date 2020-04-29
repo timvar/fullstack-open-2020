ff92173bf32475ceba7522faf43171765ff7c760
Timo Varila <timo.varila@siili.com>
2020-04-29 17:33:10 +0300

    9.1

---
+++ osa9/tsharj1/bmiCalculator.ts
@@ -0,0 +1,16 @@
+
+const calculateBmi = (height : number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+  if (bmi >= 18.5 && bmi < 25) {
+    return 'Normal (healthy weight)';
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'Overweight';
+  } else if (bmi >= 30) {
+    return 'Obese';
+  } else {
+    return 'Underweight';
+  }
+}
+
+console.log(calculateBmi(180,74))
