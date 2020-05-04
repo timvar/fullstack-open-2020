4e3ba44d0752bfcdac280d07b6947d39e1d3ef00
Timo Varila <timo.varila@siili.com>
2020-05-04 17:05:55 +0300

    9.12

--- osa9/backend-patientor/src/routes/patients.ts
+++ osa9/backend-patientor/src/routes/patients.ts
@@ -7,4 +7,24 @@ router.get('/', (_req, res) => {
   res.send(patientService.getEntries());
 });
 
+router.post('/', (req, res) => {
+  const {
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation,
+  } = req.body;
+
+  const newPatient = patientService.addEntry(
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  );
+  
+  res.json(newPatient);
+});
+
 export default router;
--- osa9/backend-patientor/src/services/patientService.ts
+++ osa9/backend-patientor/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patients from '../../data/patients';
 import {Patient} from '../types';
+import {v4 as uuidv4} from 'uuid';
 
 const getEntries = (): Pick<Patient, 'id' | 'name' | 'dateOfBirth' | 'gender' | 'occupation'>[] => {
   return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
@@ -11,8 +12,24 @@ const getEntries = (): Pick<Patient, 'id' | 'name' | 'dateOfBirth' | 'gender' | 
   }));
 };
 
-const addEntry = () => {
-  return [];
+const addEntry = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: string,
+  occupation: string,
+): Patient => {
+  const newPatientEntry = {
+    id: uuidv4(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  };
+  patients.push(newPatientEntry);
+
+  return newPatientEntry;
 };
 
 export default {
