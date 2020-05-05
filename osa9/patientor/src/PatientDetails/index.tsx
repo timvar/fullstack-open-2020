d76381d0a483f366e3baa3314348825ef73229c1
Timo Varila <timo.varila@siili.com>
2020-05-05 17:06:46 +0300

    9.17

---
+++ osa9/patientor/src/PatientDetails/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import { Patient, Gender } from "../types";
+import { Icon } from "semantic-ui-react";
+import axios from 'axios';
+import { apiBaseUrl } from "../constants";
+
+const PatientDetails: React.FC = () => {
+  const {id} = useParams<{id: string}>();
+  const [patient, setPatient] = React.useState<Patient | undefined>();
+  React.useEffect(() => {
+
+  const fetchPatient = async (patientId: string) => {
+    try {
+      const {data: patient} = await axios.get<Patient>(`${apiBaseUrl}/patients/${patientId}`);
+      setPatient(patient);
+    }
+    catch (e) {
+      console.error(e);
+    }
+    
+  };
+  
+  fetchPatient(id);
+  }, [id]);
+
+  const genderSymbol = (gender: Gender | undefined) => {
+    switch (gender) {
+      case 'male':
+        return 'mars';
+      case 'female':
+        return 'venus';
+      default:
+        return 'genderless';
+    }
+  };
+  
+  return (
+    <>
+      <h2> {patient ? patient.name : null} <Icon name={genderSymbol(patient?.gender)} />  </h2>
+      <p> ssn: {patient ? (patient.ssn ? patient.ssn : null) : null} </p>
+      <p> occupation: {patient ? patient.occupation : null} </p>
+    </>
+  );
+};
+
+export default PatientDetails;
+
