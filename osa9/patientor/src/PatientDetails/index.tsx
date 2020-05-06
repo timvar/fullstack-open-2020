3ab999dd53e27af210268d7b8a8f4773716c8c68
Timo Varila <timo.varila@siili.com>
2020-05-06 15:00:51 +0300

    9.21

--- osa9/patientor/src/PatientDetails/index.tsx
+++ osa9/patientor/src/PatientDetails/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
-import { Patient, Gender } from "../types";
+import { Patient, Gender, Diagnosis } from "../types";
 import { Icon } from "semantic-ui-react";
 import axios from 'axios';
 import { apiBaseUrl } from "../constants";
 
 const PatientDetails: React.FC = () => {
   const {id} = useParams<{id: string}>();
+  const [diagnoses, setDiagnoses] = React.useState<Diagnosis[]>();
   const [patient, setPatient] = React.useState<Patient | undefined>();
   React.useEffect(() => {
 
@@ -20,7 +21,19 @@ const PatientDetails: React.FC = () => {
     }
     
   };
-  
+
+  const fetchDiagnosisList = async () => {
+    try {
+      const {data: diagnosisList} = await axios.get<Diagnosis[]>(
+        `${apiBaseUrl}/diagnoses`
+      );
+      setDiagnoses(diagnosisList);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  fetchDiagnosisList();
   fetchPatient(id);
   }, [id]);
 
@@ -47,7 +60,7 @@ const PatientDetails: React.FC = () => {
             <p key={`${index}-${item.id}`}>{item.date} {item.description} </p>
             {item.diagnosisCodes ? (
               item.diagnosisCodes.map((d,idx) => {
-                return (<li key={idx}>{d}</li>);
+              return (<li key={idx}>{d} {(diagnoses?.find((diag: Diagnosis) => diag.code === d))?.name}</li>);
               })
             )
             :
--- osa9/patientor/src/state/reducer.ts
+++ osa9/patientor/src/state/reducer.ts
@@ -1,8 +1,9 @@
 import { State } from "./state";
-import { Patient } from "../types";
+import { Patient, Diagnosis } from "../types";
 
 export const SET_PATIENT_LIST ="SET_PATIENT_LIST";
 export const ADD_PATIENT = "ADD_PATIENT";
+export const SET_DIAGNOSIS_LIST = "SET_DIAGNOSIS_LIST";
 
 interface SetPatientListAction {
   type: typeof SET_PATIENT_LIST;
@@ -14,7 +15,12 @@ interface AddPatientAction {
   payload: Patient;
 }
 
-export type Action = SetPatientListAction | AddPatientAction;
+interface SetDiagnosisListAction {
+  type: typeof SET_DIAGNOSIS_LIST;
+  payload: Diagnosis[];
+}
+
+export type Action = SetPatientListAction | AddPatientAction | SetDiagnosisListAction;
 
 export function setPatientList(patientList: Patient[]): Action {
   return {
@@ -23,9 +29,16 @@ export function setPatientList(patientList: Patient[]): Action {
   };
 }
 
+export function setDiagnosisList(diagnosisList: Diagnosis[]): Action {
+  return {
+    type: SET_DIAGNOSIS_LIST,
+    payload: diagnosisList
+  };
+}
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case SET_PATIENT_LIST:
       return {
         ...state,
         patients: {
@@ -36,12 +49,23 @@ export const reducer = (state: State, action: Action): State => {
           ...state.patients
         }
       };
-    case "ADD_PATIENT":
+    case ADD_PATIENT:
       return {
         ...state,
         patients: {
           ...state.patients,
           [action.payload.id]: action.payload
+        }
+      };
+    case SET_DIAGNOSIS_LIST:
+      return {
+        ...state,
+        diagnoses: {
+          ...action.payload.reduce(
+            (memo, diagnosis) => ({ ...memo, [diagnosis.code]: diagnosis }),
+            {}
+          ),
+          ...state.diagnoses
         }
       };
     default:
