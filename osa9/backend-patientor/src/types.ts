92a9b62863145a4557cdd644e0f7e744591f8211
Timo Varila <timo.varila@siili.com>
2020-05-06 11:26:08 +0300

    9.19

--- osa9/backend-patientor/src/types.ts
+++ osa9/backend-patientor/src/types.ts
@@ -1,13 +1,3 @@
-export interface Diagnose {
-  code: string;
-  name: string;
-  latin?: string;
-} 
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export interface Patient {
   id: string;
   name: string;
@@ -18,13 +8,64 @@ export interface Patient {
   entries: Entry[];
 }
 
+export enum Gender {
+  female = 'female',
+  male = 'male',
+  other = 'other'
+}
+
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+} 
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Diagnosis {
+  code: string;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+} 
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  diagnosisCodes: Array<Diagnosis['code']>;
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry = HealthCheckEntry | OccupationalHealthCareEntry | HospitalEntry;
+
 export type NewPatientEntry = Omit<Patient, 'id'>;
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >; 
 
-export enum Gender {
-  'female',
-  'male',
-  'other'
-}
 
